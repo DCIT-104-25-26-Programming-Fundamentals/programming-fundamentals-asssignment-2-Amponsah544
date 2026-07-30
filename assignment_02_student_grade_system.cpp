1a83118d76a9577e20172f66bc0fa7eb1bf063d4
dennisamponsah378@gmail.com <dennisamponsah378@gmail.com>
2026-07-30 19:26:02 +0000

    Second Assignment

--- assignment_02_student_grade_system.cpp
+++ assignment_02_student_grade_system.cpp
@@ -46,3 +46,49 @@
 #include <iostream>
 using namespace std;
 
+char getGrade(int score)
+{
+    if (score < 0 || score > 100)
+    {
+        return 0;
+    }
+    else if (score >= 80)
+    {
+        return 'A';
+    }
+    else if (score >= 70)
+    {
+        return 'B';
+    }
+    else if (score >= 60)
+    {
+        return 'C';
+    }
+    else if (score >= 50)
+    {
+        return 'D';
+    }
+    else
+    {
+        return 'F';
+    }
+}
+
+int main()
+{
+    int score;
+    std::cout << "Enter your score: ";
+    std::cin >> score;
+
+    char grade = getGrade(score);
+
+    if (grade == '\0')
+    {
+        std::cout << "Error Score must be between 0 and 100: " << std::endl;
+    }
+    else
+    {
+        std::cout << "Grade: " << grade << std::endl;
+    }
+    return 0;
+}
